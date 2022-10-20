c5ac9d870ddc090119c97a89615f6af7db64e6ce
Alif Amarta <115516820+alifamarta@users.noreply.github.com>
2022-10-20 13:02:28 +0700

    Update Latihan3.py

--- Latihan3.py
+++ Latihan3.py
@@ -6,13 +6,11 @@ b = input("Masukkan nilai b: ")
 print("Variabel a =", a)
 print("Variabel b =",b)
 
+# cetak hasil operasi kedua variable dengan string format
+print("Hasil penggabung {1}&{0}=%d".format(a,b) %(a&b))
+
 #konversi nilai variable
 a = int(a)
 b = int(b)
-
-#print hasil operasi kedua variable dengan string format
-a = int(a)
-b = int(b)
-print("Hasil penggabung {1}&{0}=%d".format(a,b) %(a&b))
 print("Hasil penjumlahan {1}+{0}=%d".format(a,b) %(a+b))
 print("Hasil pembagian {1}/{0}=%d.".format(a,b) %(a/b))
